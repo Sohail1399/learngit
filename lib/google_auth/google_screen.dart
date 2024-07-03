eb3f4d10cff894c18269a29cfab4dad720f704b9
Sohail1399 <sohail.edugaon@gmail.com>
2024-07-03 15:48:11 +0530

    add google

---
+++ lib/google_auth/google_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GoogleScreen1 extends StatefulWidget {
+  const GoogleScreen1({super.key});
+
+  @override
+  State<GoogleScreen1> createState() => _GoogleScreen1State();
+}
+
+class _GoogleScreen1State extends State<GoogleScreen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("nvbfdmgf,bgf"),),
+    );
+  }
+}
